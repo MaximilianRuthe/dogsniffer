3c7e2f054c1bcd48d008808b2d547afa2175c4f5
Maximilian Ruthe <Max@maximusbusiness.com>
2014-10-20 11:21:17 -0700

    Reworked and Updated to prototype #2

--- js/druid.js
+++ js/druid.js
@@ -1,25 +1,25 @@
 var app = {
 
 	init: function() {
-		app.getPosts();
+		app.getPosts('listing');
 	},
 
-	getPosts: function() {
-
+	getPosts: function(type) {
 		var rootURL = 'http://dogsniffer.com/la/wp-json';
-
+		
 		$.ajax({
 			type: 'GET',
-			url: rootURL + '/posts?type=listing',
+			url: rootURL + '/posts?type='+type+'&filter[posts_per_page]=3',
 			dataType: 'json',
 			success: function(data){
 				
 				$.each(data, function(index, value) {
-					console.log(value.featured_image);
-			      $('ul.druid-listings').append('<li class="druid-list-item">' +
-			      	'<img src="'+value.featured_image.attachment_meta.sizes.medium.url+'" /><br>' +
-			      	'<h3>'+value.title+'</h3>' +
-			      	'<p>'+value.excerpt+'</p></li>');
+					//console.log(value.featured_image);
+			      $('ul.druid-listings').append('<li class="druid-list-item bubble row box item-'+type+'">' +
+			      	'<div class="col-xs-2 col-sm-2 col-md-2"><img src="'+value.featured_image.attachment_meta.sizes.thumbnail.url+'" class="thumbnail" /></div>' +
+			      	'<div class="col-xs-6 col-sm-6 col-md-6"><h3>'+value.title+'</h3>' +'<p>'+value.excerpt+'</p></div>' +
+					'<div class="col-xs-4 col-sm-4 col-md-4">'+'</div>' +
+					'</li>');
 			    });
 			},
 			error: function(error){
@@ -29,5 +29,13 @@ var app = {
 		});
 
 	}
-
+	
 }
+jQuery(function($) {
+	$('#show-events').click(function(){
+		app.getPosts('event');
+	});
+	$('#show-listings').click(function(){
+		app.getPosts('listing');
+	});
+});
